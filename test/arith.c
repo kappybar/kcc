2725af3cfa25c3cb6c6467551d215e153e992266
kappybar <newton7233@outlook.jp>
2022-03-02 23:22:47 +0900

    modify arith type test

--- test/arith.c
+++ test/arith.c
@@ -3,20 +3,26 @@
 int main() {
     ASSERT(10, 10);
     ASSERT(5 , 2 + 3);
+    ASSERT(1,  3 - 2);
+    ASSERT(6,  3 * 2);
+    ASSERT(6 , 24 / 4);
     ASSERT(11, 5 - 3 - 1 + 10);
-    ASSERT(6 , 24 /    4);
     ASSERT(24, 6 * 3 + 6);
-    ASSERT(12,((1 + 2) + 3 + 4) * 2 / 5 * 3);
+    ASSERT(12, ((1 + 2) + 3 + 4) * 2 / 5 * 3);
     ASSERT(24, -8 * -3);
-    ASSERT(12, (  -2 *3 + + 9) * -4 * -1);
+    ASSERT(12, (-2 *3 + + 9) * -4 * -1);
     ASSERT(1 , 1 == 1);
     ASSERT(1 , 1 != 0);
     ASSERT(1 , 0 < 1);
     ASSERT(1 , 0 <= 0);
+    ASSERT(0 , 0 == 1);
+    ASSERT(0 , 1 != 1);
+    ASSERT(0 , 1 < 0);
+    ASSERT(0 , 1 <= 0);
     ASSERT(2 , 30 % 4);
     ASSERT(1 , 100 % 3);
     ASSERT(4 , (0 >= 0) + (5 > 2) + (3 == 3) + (1 != 3) + (0 < 0) + (2 <= 1) + (3 != 3) + (1 == 3));
-    ASSERT(3 , ({int a;a = + + + + + 3;a; }));
-    ASSERT(3 , ({int a;a = - + + - + - 3; a = a + 6;a; }));
+    ASSERT(3 , + + + + + 3);
+    ASSERT(3 , (- + + - + - 3) + 6);
     return 0;
 }
--- test/type.c
+++ test/type.c
@@ -1,35 +1,69 @@
 #include "test.h"
 
-short x;
+char xc1;
+char xc2 = 1;
+char *xc3 = &xc2;
+
+short xs1;
+short xs2 = 1;
+short *xs3 = &xs2;
+
+int xi1;
+int xi2 = 1;
+int *xi3 = &xi2;
+
+long xl1;
+long xl2 = 1;
+long *xl3 = &xl2;
+
+char addc(char x, char y) {
+    return x + y;
+}
+
 short adds(short x, short y) {
     return x + y;
 }
 
-long xl1;
-long xl2 = 7; 
+int addi(int x, int y) {
+    return x + y;
+}
+
 long addl(long x, long y) {
     return x + y;
 }
 
-char xc3 = 1;
-short xs3 = 1;
-int xi3 = 1;
-long xl3 = 1; 
 
 int main() {
 
+    ASSERT(4 , ({char x; x = 4; x; }));
+    ASSERT(0 , xc1);
+    ASSERT(1 , xc2);
+    ASSERT(1 , *xc3);
+    ASSERT(6 , ({char x = adds(1,2); char y = adds(x,3); y;}));
+    ASSERT(4 , ({char x, *y = &x; *y = 4; x; }));
+    ASSERT(10, ({char arr[10]; int i;for (i = 0;i < 10; i=i+1) arr[i] = 1;char sum = 0;for (i = 0;i < 10;i = i+1) sum = sum + arr[i]; sum; }));
     ASSERT(4 , ({short x; x = 4; x; }));
-    ASSERT(0 , x);
+    ASSERT(0 , xs1);
+    ASSERT(1 , xs2);
+    ASSERT(1 , *xs3);
     ASSERT(6 , ({short x = adds(1,2); short y = adds(x,3); y;}));
-    ASSERT(4 , ({ short x, *y = &x; *y = 4; x; }));
+    ASSERT(4 , ({short x, *y = &x; *y = 4; x; }));
     ASSERT(10, ({ short arr[10]; int i;for (i = 0;i < 10; i=i+1) arr[i] = 1;short sum = 0;for (i = 0;i < 10;i = i+1) sum = sum + arr[i]; sum; }));
+    ASSERT(4 , ({int x; x = 4; x; }));
+    ASSERT(0 , xi1);
+    ASSERT(1 , xi2);
+    ASSERT(1 , *xi3);
+    ASSERT(6 , ({int x = adds(1,2); short y = adds(x,3); y;}));
+    ASSERT(4 , ({int x, *y = &x; *y = 4; x; }));
+    ASSERT(10, ({ int arr[10]; int i;for (i = 0;i < 10; i=i+1) arr[i] = 1;short sum = 0;for (i = 0;i < 10;i = i+1) sum = sum + arr[i]; sum; }));
     ASSERT(4 , ({long x; x = 4; x; }));
     ASSERT(0 , xl1);
-    ASSERT(7 , xl2);
+    ASSERT(1 , xl2);
+    ASSERT(1 , *xl3);
     ASSERT(6 , ({long x = addl(1,2); long y = addl(x,3); y; }));
     ASSERT(4 , ({ long x, *y = &x; *y = 4; x; }));
     ASSERT(10, ({ long arr[10]; int i;for (i = 0;i < 10; i=i+1) arr[i] = 1;long sum = 0;for (i = 0;i < 10;i = i+1) sum = sum + arr[i]; sum;}));
-    ASSERT(4 , xc3 + xs3 + xi3 + xl3);
+
     ASSERT(1 , ({char y; long x = sizeof(y); x;}));
     ASSERT(2 , ({short y; long x = sizeof(y); x;}));
     ASSERT(4 , ({int y; long x = sizeof(y); x;}));
